05bdb280deab9091ffa004fd0b14e7c51a077dbe
Jubins <jubins@nyu.edu>
2016-07-14 16:07:05 -0400

    Updated Day 9 recursion to calculate the Exponent for biggest of the numbers.
    Updated Day 9 recursion to calculate the Exponent for biggest of the
    numbers.
    Hacker Rank Solution to 30 Days of Code in Java.

--- Day 9/Recursion.java
+++ Day 9/Recursion.java
@@ -1,11 +1,12 @@
 //Day 9: Recursion Solution. 30- Days-of-Code Hackerrank Solution
-//This program calculates Factorial(eg: 8!), Summation(eg: E(3) = 3+2+1) & Exponentiation(eg: 3^2 = 3*3). However, comment out the Summation and Exponentiatio nmethods as for this challenge you only need the Factorial.
+//This program calculates Factorial(eg: 8!), Summation(eg: E(3) = 3+2+1) & Exponentiation(eg: 3^2 = 3*3) for biggest of the numbers. However, comment out the Summation and Exponentiatio nmethods as for this challenge you only need the Factorial.
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class Recursion {
 	
-	public static int Factorial(int n){
+	public static long Factorial(long n){
 		//BASE CASE
 		if(n<=1){
 			return 1;
@@ -19,7 +20,7 @@ public class Recursion {
 		}
 	}
 	
-	public static int Summation(int n){
+	public static long Summation(long n){
 		//BASE CASE
 		if(n<=0){
 			return 0;
@@ -30,17 +31,20 @@ public class Recursion {
 		}
 	}
 
-	public static int Exponentiation(int n,int p){
+	public static BigInteger Exponentiation(long n,long p){
 		//BASE CASE
 		if(p<=0){
-			return 1;
+			BigInteger returnOne = new BigInteger("1");
+			return returnOne;
 		}
 		//RECURSIVE CASE: 5^3 = 5*Exponentiation(5,2)
 		//5^3 = 5*5*Exponentiation(5,1)
 		//5^3 = 5*5*5*Exponentiation(5,0)
 		//5^3 = 5*5*5*1
 		else{
-			return n*Exponentiation(n, p-1);
+			BigInteger calculateExponent = Exponentiation(n, p-1);
+			BigInteger valueOfExponent = BigInteger.valueOf(n);
+			return valueOfExponent.multiply(calculateExponent);
 		}
 	}
 	
@@ -48,8 +52,8 @@ public class Recursion {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     	@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
-    	int n = in.nextInt();
-    	//int p = in.nextInt();
+    	long n = in.nextInt();
+    	//long p = in.nextInt(); //Uncomment this to calculateExponents
     	
     	//System.out.println(Factorial(n));
     	System.out.println(n+"! = "+Factorial(n));
