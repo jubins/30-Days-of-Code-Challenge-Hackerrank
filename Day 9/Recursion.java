2e231dd8fbc28569820d899cb203403b0856c6b0
Jubins <jubins@nyu.edu>
2016-07-14 16:34:04 -0400

    Updated Day 9 Recursion to calculate Factorials and Exponents for very big numbers in Java
    Hacker Rank 30 Days of Code Solution in Java

--- Day 9/Recursion.java
+++ Day 9/Recursion.java
@@ -1,22 +1,25 @@
 //Day 9: Recursion Solution. 30- Days-of-Code Hackerrank Solution
-//This program calculates Factorial(eg: 8!), Summation(eg: E(3) = 3+2+1) & Exponentiation(eg: 3^2 = 3*3) for biggest of the numbers. However, comment out the Summation and Exponentiatio nmethods as for this challenge you only need the Factorial.
+//This program calculates Factorial(eg: 8!), Summation(eg: E(3) = 3+2+1) & Exponentiation(eg: 3^2 = 3*3). However, comment out the Summation and Exponentiatio nmethods as for this challenge you only need the Factorial.
 
 import java.math.BigInteger;
 import java.util.*;
 
-public class Recursion {
+public class testsums {
 	
-	public static long Factorial(long n){
+	public static BigInteger Factorial(long n){
 		//BASE CASE
 		if(n<=1){
-			return 1;
+			BigInteger returnOne = new BigInteger("1");
+			return returnOne;
 		}
 		//RECURSIVE CASE: 3! = 3*Factorial(3-1)
 		//3! = 3*2*Factorial(2-1)
 		//3! = 3*2*Factorial(1)
 		//3! = 3*2*1
 		else{
-			return n*Factorial(n-1);
+			BigInteger number = BigInteger.valueOf(n);
+			BigInteger decreasedNumber = Factorial(n-1);
+			return number.multiply(decreasedNumber);
 		}
 	}
 	
@@ -42,9 +45,9 @@ public class Recursion {
 		//5^3 = 5*5*5*Exponentiation(5,0)
 		//5^3 = 5*5*5*1
 		else{
-			BigInteger calculateExponent = Exponentiation(n, p-1);
-			BigInteger valueOfExponent = BigInteger.valueOf(n);
-			return valueOfExponent.multiply(calculateExponent);
+			BigInteger number = BigInteger.valueOf(n);
+			BigInteger power = Exponentiation(n, p-1);
+			return number.multiply(power);
 		}
 	}
 	
@@ -55,9 +58,9 @@ public class Recursion {
     	long n = in.nextInt();
     	//long p = in.nextInt(); //Uncomment this and last output line to calculateExponents
     	
-    	//System.out.println(Factorial(n));
-    	System.out.println(n+"! = "+Factorial(n));
-    	System.out.println("E("+n+") = "+Summation(n));
-    	//System.out.println(n+"^"+p+" = "+Exponentiation(n, p)); Uncomment this to calculateExponents
+    	
+    	//System.out.println(n+"! = "+Factorial(n)); //Uncomment this to calculateFactorial
+    	System.out.println("Sigma("+n+") = "+Summation(n));
+    	//System.out.println(n+"^"+p+" = "+Exponentiation(n, p)); //Uncomment this to calculateExponents
     }
 }
